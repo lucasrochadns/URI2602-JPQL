4f813a28fc6bb55c387281c8549c85040407fc46
Lucas Rocha <lucas.rocha-dev@outlook.com>
2023-01-09 18:24:36 -0300

    TEST

--- src/main/java/com/devsuperior/uri2602/Uri2602Application.java
+++ src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -1,12 +1,28 @@
 package com.devsuperior.uri2602;
 
+import com.devsuperior.uri2602.projections.CustmoerMinProjection;
+import com.devsuperior.uri2602.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class Uri2602Application {
+public class Uri2602Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2602Application.class, args);
+	}
+
+    @Autowired
+	private CustomerRepository repository;
+	@Override
+	public void run(String... args) throws Exception {
+
+		repository.searchByNameWhereState("RS").stream().map(x -> x.getName()).forEach(System.out::println);
+		List<CustmoerMinProjection> names = repository.searchByNameWhereState("MG");
+		names.stream().map(x -> x.getName()).forEach(System.out::println);
 	}
 }
--- src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
+++ src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
@@ -1,10 +1,17 @@
 package com.devsuperior.uri2602.repositories;
 
 import com.devsuperior.uri2602.entities.Customer;
+import com.devsuperior.uri2602.projections.CustmoerMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
-    
+
+    /**CONSULTA CUSTOMIZADA -> PROJECTIONS*/
+    @Query(nativeQuery = true, value="SELECT name FROM customers WHERE state= :state")
+   List<CustmoerMinProjection> searchByNameWhereState(String state);
 }
